cb65e6230e130e2904cffba6c721d503637b7082
TestLeafPages <sarath.muthuswamy@qeagle.com>
2022-06-14 10:20:06 +0530

    Add files via upload

---
+++ qa/ReverseChar.java
@@ -0,0 +1,27 @@
+package qa;
+
+import java.util.Arrays;
+
+public class ReverseChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nm = "de12ef35xy";
+
+		char[] charArray = nm.toCharArray();
+		char[] charArray1 = new char[charArray.length];
+
+		for (int i = 0; i < charArray.length; i++) {
+
+			if (Character.isAlphabetic(charArray[i])) {
+				charArray1[charArray.length - i - 1] = charArray[i];
+			} else {
+				charArray1[i] = charArray[i];
+			}
+
+		}
+		System.out.print(Arrays.toString(charArray1));
+	}
+
+}
